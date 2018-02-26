c3c8655270124d519a0c2200e0246ff509fc273f
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-02-25 19:22:46 -0500

    Add Customer class and RandomMovieViewModel

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,12 +14,18 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
 
-            return View(movie);
-            //return Content("Hello World!");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" }); // (name of action, controller, anonymous object) ---> redirects to URL containing /?page=1&sortBy=name
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
         }
 
         public ActionResult Edit(int id) // id is same as in Controller
