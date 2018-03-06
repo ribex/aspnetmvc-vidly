cc5258da4e7d570745ebec54fd71dfef75021ee3
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-03-05 20:06:19 -0500

    Change customer view to database instead of hardcoded values

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -8,17 +8,29 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
             {
@@ -26,17 +38,6 @@ namespace Vidly.Controllers
             }
 
             return View(customer);
-        }
-
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer { Name = "Joan Rivers", Id = 1 },
-                new Customer { Name = "Snoopy", Id = 2 },
-                new Customer { Name = "Glenn Close", Id = 3 },
-                new Customer { Name = "Meryl Streep", Id = 4 }
-            };
         }
     }
 }
