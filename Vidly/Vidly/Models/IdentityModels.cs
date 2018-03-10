8afb28aa566225ddb756bd5d35876917e1dca866
Rebecca Wagaman <rebecca.wagaman@gmail.com>
2018-03-10 11:36:05 -0500

    Add Movie DbSet to Context, add movie properties, add Genre class, add migration, update database

--- Vidly/Vidly/Models/IdentityModels.cs
+++ Vidly/Vidly/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Vidly.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public DbSet<Customer> Customers { get; set; }
-        // TODO: add DbSet<Movie> Movies
+        public DbSet<Movie> Movies { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
